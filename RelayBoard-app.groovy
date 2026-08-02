84027a33991e600d17986e8d072edfe6ba20405b
TonyMajorDev <39914475+TonyMajorDev@users.noreply.github.com>
2026-08-02 10:35:53 -0400

    updated year

--- RelayBoard-app.groovy
+++ RelayBoard-app.groovy
@@ -1,7 +1,7 @@
 /**
  *  RIB App (Event) for Hubitat
  *
- *  Copyright 2023 ValkyrieTech LLC
+ *  Copyright 2026 ValkyrieTech LLC
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
--- RelayBoard-contact-sensor-driver.groovy
+++ RelayBoard-contact-sensor-driver.groovy
@@ -1,7 +1,7 @@
 /**
  *  RIB Contact Sensor
  *
- *  Copyright 2022 ValkyrieTech LLC
+ *  Copyright 2026 ValkyrieTech LLC
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
